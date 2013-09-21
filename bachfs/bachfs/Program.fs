fa46a96a44c69a404ed4a68a10ea65653722b7b3
lefthandedgoat <lefthandedgoat@gmail.com>
2013-09-21 11:44:49 -0500

    sinOsc created

--- bachfs/bachfs/Program.fs
+++ bachfs/bachfs/Program.fs
@@ -1,1 +1,28 @@
-﻿
+﻿module main
+
+//#r @"C:\projects\bachfs\bachfs\packages\Bespoke-OSC-Library.1.0.0\lib\Bespoke.Common.dll"
+//#r @"C:\projects\bachfs\bachfs\packages\Bespoke-OSC-Library.1.0.0\lib\Bespoke.Common.Osc.dll"
+
+open System
+open sc
+
+///////////////////////////////////////////////////////////////
+// Originally by:                                            //
+// Functional Composition by Chris Ford (@ctford)            //
+// ThoughtWorks Uganda                                       //
+//                                                           //
+// http://github.com/ctford/functional-composition           //
+// http://github.com/ctford/leipzig                          //
+// http://github.com/overtone/overtone                       //
+//                                                           //
+// Converted to f# by Chris Holt                             //
+// @lefthandedgoat                                           //
+// http://github.com/lefthandedgoat/                         //
+///////////////////////////////////////////////////////////////
+
+sinOsc 300
+sinOsc 400
+sinOsc 500
+
+
+System.Console.ReadKey()
