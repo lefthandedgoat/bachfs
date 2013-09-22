7378e4a4062d1699c653d9a972407810087c2733
lefthandedgoat <lefthandedgoat@gmail.com>
2013-09-22 14:31:11 -0500

    bell withouth detect silence

--- bachfs/bachfs/Program.fs
+++ bachfs/bachfs/Program.fs
@@ -41,11 +41,32 @@ let doubleTone freq1 freq2 =
     tone freq1
     tone freq2
 
-let beep freq duration = Envelope(1, 0, duration, (SinOsc freq)) |> play
+let beep freq duration = Envelope(1.0, 0.0, duration, (SinOsc freq)) |> play
 
 tone 300.0
 doubleTone 300.0 300.0
-stop()
 beep 300.0 1.0
+stop()
 
-System.Console.ReadKey()
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+// Harmonics                                                 //
+///////////////////////////////////////////////////////////////
+
+let bell freq duration harmonics =
+    let defaultHarmonics = [1.0; 0.6; 0.4; 0.25; 0.2; 0.15]
+    let harmonicSeries = [1.0; 2.0; 3.0; 4.0; 5.0; 6.0]
+    let proportions = 
+        harmonics @ (Seq.skip (List.length harmonics) defaultHarmonics |> List.ofSeq)
+    let component' harmonic proportion =
+        PercEnvelope(0.01, (proportion * duration), 1.0, -4.0, proportion, (SinOsc (harmonic * freq)))
+    let whole = 
+        Mix (List.map2 component' harmonicSeries proportions)
+    DetectSilence whole |> play
+
+
+bell 300.0 10.0 []
--- bachfs/bachfs/sc.fs
+++ bachfs/bachfs/sc.fs
@@ -4,9 +4,18 @@ open Bespoke.Common.Osc
 open System.Net
 open System
 
+type level = float
+type attack = float
+type release = float
+type curve = float
+type proportion = float
+
 type command =
     | SinOsc of float
-    | Envelope of int * int * float * command
+    | Envelope of level * level * attack * command
+    | PercEnvelope of attack * release * level * curve * proportion * command
+    | Mix of command list
+    | DetectSilence of command
     
 OscPacket.LittleEndianByteOrder <- false
 
@@ -18,6 +27,8 @@ let nextId = (fun () -> id := !id + 1; !id)
     
 let mutable playing : int list = []
 
+let private tofloat32 (f : float) = f |> Convert.ToSingle
+
 let stop () =
     playing
     |> List.iter (fun i ->
@@ -28,35 +39,57 @@ let stop () =
 
 let private sinOsc (freq : float) =                
     let id = nextId()
-    let freq = freq |> Convert.ToSingle
-
+    
     let msg = new OscMessage(superCollider, "/s_new")
     msg.Append("default") |> ignore
     msg.Append(id) |> ignore
     msg.Append(1) |> ignore
     msg.Append(0) |> ignore
     msg.Append("freq") |> ignore
-    msg.Append(freq) |> ignore
+    msg.Append(tofloat32 freq) |> ignore
         
     playing <- playing @ [id]
     msg.Send(superCollider)
     
-let private envelope start stop (duration : float) command =
-    let envelopedSinOsc (freq : float) =
-        let freq = freq |> Convert.ToSingle
-        let duration = duration |> Convert.ToSingle
+let private envelope start stop duration command =
+    let envelopedSinOsc (freq : float) =        
         let msg = new OscMessage(superCollider, "/envelopedSinOsc")    
-        msg.Append(start) |> ignore
-        msg.Append(stop) |> ignore
-        msg.Append(duration) |> ignore
-        msg.Append(freq) |> ignore        
+        msg.Append(tofloat32 start) |> ignore
+        msg.Append(tofloat32 stop) |> ignore
+        msg.Append(tofloat32 duration) |> ignore
+        msg.Append(tofloat32 freq) |> ignore        
         msg.Send(superColliderLanguage)
 
     match command with
     | SinOsc(freq) -> envelopedSinOsc freq
     | _ -> failwith "does not compute!"
+
+let private detectSilence command =
+    let matchPercEnvelope (commands : command list) (msg : OscMessage) =
+        let addPercEnvelop perc =
+            match perc with
+            | PercEnvelope(attack, release, level, curve, proportion, SinOsc(freq)) ->
+                msg.Append(tofloat32 attack) |> ignore
+                msg.Append(tofloat32 release) |> ignore
+                msg.Append(tofloat32 level) |> ignore
+                msg.Append(tofloat32 curve) |> ignore
+                msg.Append(tofloat32 freq) |> ignore
+                msg.Append(tofloat32 proportion) |> ignore                
+        
+        let count = List.length commands
+        msg.Append(count) |> ignore
+        commands |> List.iter addPercEnvelop
+        msg.Send(superColliderLanguage)
+
+    match command with
+    | Mix(commands) ->
+        match commands with
+        | PercEnvelope(_,_,_,_,_,_) :: _ -> matchPercEnvelope commands (new OscMessage(superCollider, "/mixedPercEnvelopes"))
+        | _ -> failwith "boom"
+    | _ -> failwith "booooom"
     
 let play command =
     match command with
     | SinOsc(freq) -> sinOsc freq
     | Envelope(start, stop, duration, command) -> envelope start stop duration command
+    | DetectSilence(command) -> detectSilence command
